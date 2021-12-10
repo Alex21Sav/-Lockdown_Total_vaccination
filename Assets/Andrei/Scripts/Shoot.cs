a1ce1ea085f9436c55cdb9cb23eabe168be5df4f
Alex <savostin2133@gmail.com>
2021-12-11 01:42:26 +0700

    -

--- Assets/Andrei/Scripts/Shoot.cs
+++ Assets/Andrei/Scripts/Shoot.cs
@@ -14,9 +14,10 @@ public class Shoot : MonoBehaviour
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))&&_canShoot)
         {
             GameObject syringe = Instantiate(_syringe, transform.position, Quaternion.Euler(90, -90, 0));
+            //GameObject syringe = Instantiate(_syringe, transform.position, Quaternion.LookRotation(transform.position, Vector3.forward));
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.y = 1;
-            syringe.GetComponent<Rigidbody>().AddForce((mousePosition - transform.position).normalized * _shootSpeed, ForceMode.Impulse);
+            syringe.GetComponent<Rigidbody>().AddForce((mousePosition - transform.position).normalized * _shootSpeed, ForceMode.Impulse);            
             _canShoot = false;
             StartCoroutine(Reload());
         }
